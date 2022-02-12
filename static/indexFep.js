d8820a874e7e2326a9c1eef304992f466bdc17ec
E-kenny <ekennyobiasogu@gmail.com>
2022-02-12 12:39:28 +0100

    annaully

--- static/indexFep.js
+++ static/indexFep.js
@@ -1,17 +1,32 @@
 function fep() {
-    const age = document.getElementById("");
-    const duration = document.getElementById("");
-    const sumAssured = document.getElementById("");
+    const age = document.getElementById("age");
+    const duration = document.getElementById("duration");
+    const mode = document.getElementById("mode");
+    const sumAssured = document.getElementById("sumAssured");
 
-    data = {
+    const data = {
         20: [263.31, 157.23, 116.31, 90.07], 21:[263.58, 157.38, 116.43, 90.96]
     }
 
-    let rate=''
-    alert("hello Annual Premium")
+    let rate=data[age.value][duration.value]
+    let annualPremium = (sumAssured.value * rate * mode.value/1000) + 1000
+    alert(annualPremium)
 }
 
 
 function fepSA() {
-    alert("hello sum Assured")
+    const age = document.getElementById("age");
+    const duration = document.getElementById("duration");
+    const mode = document.getElementById("mode");
+    const annualPremium = document.getElementById("annualPremium");
+    
+    const data = {
+        20: [263.31, 157.23, 116.31, 90.07], 21:[263.58, 157.38, 116.43, 90.96]
+    }
+
+    let rate=data[age.value][duration.value]
+    
+    let sumAssured = (annualPremium.value *1000/rate * mode.value) - 1000
+    alert(sumAssured)
+    
 }
