d49868035a98d1d54880daceb299de20d70d5fde
E-kenny <ekennyobiasogu@gmail.com>
2022-02-12 11:05:58 +0100

    annaully

---
+++ static/indexFep.js
@@ -0,0 +1,17 @@
+function fep() {
+    const age = document.getElementById("");
+    const duration = document.getElementById("");
+    const sumAssured = document.getElementById("");
+
+    data = {
+        20: [263.31, 157.23, 116.31, 90.07], 21:[263.58, 157.38, 116.43, 90.96]
+    }
+
+    let rate=''
+    alert("hello Annual Premium")
+}
+
+
+function fepSA() {
+    alert("hello sum Assured")
+}
