23b9ffdde38e532cf1dcd17fa9f78dc03ecc2e7f
E-kenny <ekennyobiasogu@gmail.com>
2022-02-15 09:10:06 +0100

    FEP completed

--- static/indexFep.js
+++ static/indexFep.js
@@ -94,7 +94,7 @@ function fep() {
 
         totalBonus.innerHTML= '<span class="text-blue-700 mr-4">Total bonus in ' + durability + ' years = </span>  ' + Math.round(sumAssuredInt * 4/100 * durability * frequency)
         
-        end.innerHTML= '<span class="text-blue-700 mr-4">For ' + durability + ' years you will get = </span>  ' + ((sumAssuredInt * 4/100 * durability)  + (sumAssuredInt * 25/100) + (sumAssuredInt * 25/100) + (sumAssuredInt) * frequency)
+        end.innerHTML= '<span class="text-blue-700 mr-4">For ' + durability + ' years you will get = </span>  ' + ((sumAssuredInt * 4/100 * durability * frequency)  + (sumAssuredInt * 25/100) + (sumAssuredInt * 25/100) + (sumAssuredInt))
         
       }
 
@@ -198,7 +198,7 @@ function fepSA() {
 
         totalBonus.innerHTML= '<span class="text-blue-700 mr-4">Total bonus in ' + durability + ' years = </span>  ' + Math.round(sumAssured * 4/100 * durability * frequency)
         
-        end.innerHTML= '<span class="text-blue-700 mr-4">For ' + durability + ' years you will get = </span>  ' + Math.round(((sumAssured * 4/100 * durability) + (sumAssured * 25/100) + (sumAssured * 25/100) + (sumAssured) * frequency))
+        end.innerHTML= '<span class="text-blue-700 mr-4">For ' + durability + ' years you will get = </span>  ' + Math.round(((sumAssured * 4/100 * durability * frameElement) + (sumAssured * 25/100) + (sumAssured * 25/100) + (sumAssured)))
         
       }
 
