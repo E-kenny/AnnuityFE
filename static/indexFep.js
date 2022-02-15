4e61e294885a92c9f974bb17995ad3ea3b0cda3b
E-kenny <ekennyobiasogu@gmail.com>
2022-02-15 16:54:17 +0100

    FEP completed

--- static/indexFep.js
+++ static/indexFep.js
@@ -84,14 +84,14 @@ function fep() {
 
         second.innerHTML= '<span class="text-blue-700 mr-4">Second partial maturity = </span>  ' +Math.round(sumAssuredInt * 25/100)
 
-        full.innerHTML= '<span class="text-blue-700 mr-4">full partial maturity = </span>  ' + 
+        full.innerHTML= '<span class="text-blue-700 mr-4">full partial maturity = </span> ' + 
         Math.round(sumAssuredInt)
 
-        totalPremium.innerHTML= '<span class="text-blue-700 mr-4">Total premium in ' + durability + ' years = </span>  ' + Math.round(annualPremium * durability * frequency)
+        totalPremium.innerHTML= '<span class="text-blue-700 mr-4">Total premium in ' + durability + ' years = </span> ' + Math.round(annualPremium * durability * frequency)
 
-        totalBonus.innerHTML= '<span class="text-blue-700 mr-4">Total bonus in ' + durability + ' years = </span>  ' + Math.round(sumAssuredInt * 4/100 * durability)
+        totalBonus.innerHTML= '<span class="text-blue-700 mr-4">Total bonus in ' + durability + ' years = </span> ' + Math.round(sumAssuredInt * 4/100 * durability)
         
-        end.innerHTML= '<span class="text-blue-700 mr-4">For ' + durability + ' years you will get = </span>  ' + ((sumAssuredInt * 4/100 * durability)  + (sumAssuredInt * 25/100) + (sumAssuredInt * 25/100) + (sumAssuredInt))
+        end.innerHTML= '<span class="text-blue-700 mr-4">For ' + durability + ' years you will get = </span> ' + ((sumAssuredInt * 4/100 * durability)  + (sumAssuredInt * 25/100) + (sumAssuredInt * 25/100) + (sumAssuredInt))
         
       }
 
@@ -178,23 +178,23 @@ function fepSA() {
 
 
         
-        output.innerHTML='<span class="text-blue-700 text-size-sm mr-4">Sum assured = </span>  ' + Math.round(sumAssured)
+        output.innerHTML='<span class="text-blue-700 text-size-sm mr-4">Sum assured = </span> ' + Math.round(sumAssured)
         
         bonus.innerHTML = '<span class="text-blue-700 mr-4">Bonus = </span>  ' +  Math.round(sumAssured * 4/100)
         
-        first.innerHTML = '<span class="text-blue-700 text-size-sm mr-4">first Partial maturity = </span>  ' + Math.round(sumAssured * 25/100)
+        first.innerHTML = '<span class="text-blue-700 text-size-sm mr-4">first Partial maturity = </span> ' + Math.round(sumAssured * 25/100)
 
-        second.innerHTML= '<span class="text-blue-700 mr-4">Second partial maturity = </span>  ' + 
+        second.innerHTML= '<span class="text-blue-700 mr-4">Second partial maturity = </span> ' + 
         Math.round(sumAssured * 25/100)
 
-        full.innerHTML= '<span class="text-blue-700 mr-4">full partial maturity = </span>  ' + 
+        full.innerHTML= '<span class="text-blue-700 mr-4">full partial maturity = </span> ' + 
         Math.round(sumAssured)
 
-        totalPremium.innerHTML= '<span class="text-blue-700 mr-4">Total premium in ' + durability + ' years = </span>  ' + Math.round(annualPremium.value * durability)
+        totalPremium.innerHTML= '<span class="text-blue-700 mr-4">Total premium in ' + durability + ' years = </span> ' + Math.round(annualPremium.value * durability)
 
-        totalBonus.innerHTML= '<span class="text-blue-700 mr-4">Total bonus in ' + durability + ' years = </span>  ' + Math.round(sumAssured * 4/100 * durability * frequency)
+        totalBonus.innerHTML= '<span class="text-blue-700 mr-4">Total bonus in ' + durability + ' years = </span> ' + Math.round(sumAssured * 4/100 * durability * frequency)
         
-        end.innerHTML= '<span class="text-blue-700 mr-4">For ' + durability + ' years you will get = </span>  ' + Math.round(((sumAssured * 4/100 * durability) + (sumAssured * 25/100) + (sumAssured * 25/100) + (sumAssured)))
+        end.innerHTML= '<span class="text-blue-700 mr-4">For ' + durability + ' years you will get = </span> ' + Math.round(((sumAssured * 4/100 * durability) + (sumAssured * 25/100) + (sumAssured * 25/100) + (sumAssured)))
         
       }
 
